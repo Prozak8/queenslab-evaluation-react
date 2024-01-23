85041f87d4b7d7f698cbeb6e951d66bbe43b3e1f
Zakary Howard <zakary.howard@ellos.se>
2024-01-23 14:07:07 +0100

    css positioning

--- src/credit-card/CreditCard.tsx
+++ src/credit-card/CreditCard.tsx
@@ -6,15 +6,31 @@ type CreditCardProps = {
 };
 
 const CreditCard = ({ cardNumber, cardHolder }: CreditCardProps) => {
-    return <div className='card'>
-      <form action="">
-        <input type="text" name="cardNumber" id="cardNumber" />
-        <input type="text" name="cardHolder" id="cardHolder" />
-        <select name="expirationMonth" id="expirationMonth"></select>
-        <select name="expirationYear" id="expirationYear"></select>
-        <input type="text" name="CVV" id="CVV" />
-      </form>
-    </div>;
+    return (
+        <div className='card'>
+            <form action='' className='card__form'>
+                <div className='input-group'>
+                    <label htmlFor='cardNumber'>Card Number</label>
+                    <input type='text' name='cardNumber' id='cardNumber' />
+                </div>
+                <div className='input-group'>
+                    <label htmlFor='cardHolder'>Card Name</label>
+                    <input type='text' name='cardHolder' id='cardHolder' />
+                </div>
+                <div className='row'>
+                    <div className='input-group col-8 row wrap'>
+                        <label htmlFor='expirationMonth'>Expiration Date</label>
+                        <select name='expirationMonth' id='expirationMonth'></select>
+                        <select name='expirationYear' id='expirationYear'></select>
+                    </div>
+                    <div className='input-group col-4'>
+                        <label htmlFor='CVV'>CVV</label>
+                        <input type='text' name='CVV' id='CVV' />
+                    </div>
+                </div>
+            </form>
+        </div>
+    );
 };
 
 export default CreditCard;
