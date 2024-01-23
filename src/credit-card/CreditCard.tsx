c05a70666e5bbf471d2572c0137fed100065417b
Zakary Howard <zakary.howard@ellos.se>
2024-01-23 16:27:35 +0100

    spacing and positioning

--- src/credit-card/CreditCard.tsx
+++ src/credit-card/CreditCard.tsx
@@ -9,22 +9,32 @@ const CreditCard = ({ cardNumber, cardHolder }: CreditCardProps) => {
     return (
         <div className='card'>
             <form action='' className='card__form'>
-                <div className='input-group'>
-                    <label htmlFor='cardNumber'>Card Number</label>
+                <div className='input-group mb-2'>
+                    <label className='input-label' htmlFor='cardNumber'>
+                        Card Number
+                    </label>
                     <input type='text' name='cardNumber' id='cardNumber' />
                 </div>
-                <div className='input-group'>
-                    <label htmlFor='cardHolder'>Card Name</label>
+                <div className='input-group mb-2'>
+                    <label className='input-label' htmlFor='cardHolder'>
+                        Card Name
+                    </label>
                     <input type='text' name='cardHolder' id='cardHolder' />
                 </div>
-                <div className='row'>
-                    <div className='input-group col-8 row wrap'>
-                        <label htmlFor='expirationMonth'>Expiration Date</label>
-                        <select name='expirationMonth' id='expirationMonth'></select>
-                        <select name='expirationYear' id='expirationYear'></select>
+                <div className='row gap-2'>
+                    <div className='input-group col-8 row wrap expiration-group'>
+                        <label className='input-label w-100' htmlFor='expirationMonth'>
+                            Expiration Date
+                        </label>
+                        <div className='expiration-select'>
+                            <select name='expirationMonth' id='expirationMonth'></select>
+                            <select name='expirationYear' id='expirationYear'></select>
+                        </div>
                     </div>
                     <div className='input-group col-4'>
-                        <label htmlFor='CVV'>CVV</label>
+                        <label className='input-label' htmlFor='CVV'>
+                            CVV
+                        </label>
                         <input type='text' name='CVV' id='CVV' />
                     </div>
                 </div>
