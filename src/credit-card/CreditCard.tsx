4451bfce130f6f0ddd550d369b76abb16eacfd56
Zakary Howard <zakary.howard@ellos.se>
2024-01-23 13:44:07 +0100

    form skeleton

--- src/credit-card/CreditCard.tsx
+++ src/credit-card/CreditCard.tsx
@@ -1,12 +1,20 @@
+import './CreditCard.css';
+
 type CreditCardProps = {
-  cardNumber: string
-  cardHolder: string
-}
+    cardNumber: string;
+    cardHolder: string;
+};
 
 const CreditCard = ({ cardNumber, cardHolder }: CreditCardProps) => {
-  return (
-    <></>
-  )
-}
+    return <div className='card'>
+      <form action="">
+        <input type="text" name="cardNumber" id="cardNumber" />
+        <input type="text" name="cardHolder" id="cardHolder" />
+        <select name="expirationMonth" id="expirationMonth"></select>
+        <select name="expirationYear" id="expirationYear"></select>
+        <input type="text" name="CVV" id="CVV" />
+      </form>
+    </div>;
+};
 
 export default CreditCard;
