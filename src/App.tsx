4049729a4eb1571a107316461d8e261f7a4b71e8
Zakary Howard <zakary.howard@ellos.se>
2024-01-29 16:53:27 +0100

    form submit logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
 import './App.css';
 import { useState } from 'react';
-import CreditCard from './credit-card/CreditCard';
+import CreditCard, { IFormState } from './credit-card/CreditCard';
 
 const App = () => {
     const [cardNumber, setCardNumber] = useState('');
     const [cardHolder, setCardHolder] = useState('');
 
+    const onSubmit = async (formData: IFormState) => {
+        const response = await fetch('/api/payments', {
+            method: 'POST',
+            body: JSON.stringify(formData),
+        });
+
+        if (response.status === 404) {
+            throw new Error('404: Not found.');
+        } else if (response.status === 500) {
+            throw new Error('500: Server error.');
+        }
+
+        const json = await response.json();
+
+        console.log(json);
+    };
+
     return (
         <div className='App'>
-            <CreditCard cardNumber={cardNumber} cardHolder={cardHolder} />
+            <CreditCard cardNumber={cardNumber} cardHolder={cardHolder} onSubmit={onSubmit} />
         </div>
     );
 };
