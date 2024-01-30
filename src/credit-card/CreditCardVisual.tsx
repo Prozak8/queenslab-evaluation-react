30e4a2a21fbcbb3871045ffe84ef022b9e9ecbcf
Zakary Howard <zakary.howard@ellos.se>
2024-01-30 10:03:32 +0100

    refactor visual logic

--- src/credit-card/CreditCardVisual.tsx
+++ src/credit-card/CreditCardVisual.tsx
@@ -1,34 +1,44 @@
-import { useCallback, useMemo } from 'react';
+import { useMemo } from 'react';
 import { IFormState } from './CreditCard';
 import './CreditCardVisual.css';
 
 type CreditCard = { name: 'visa'; numberLength: number; parts: number[] };
 type CreditCardVisualProps = React.HTMLProps<HTMLDivElement> & { formState: IFormState };
 
-const card: CreditCard = {
-    name: 'visa',
-    numberLength: 16,
-    parts: [4, 4, 4, 4],
+// Move the credit card configuration outside the component if it's constant
+// or consider passing it as a prop if it's dynamic
+const cardConfig = {
+    visa: {
+        numberLength: 16,
+        parts: [4, 4, 4, 4],
+    },
+    // Add other card types as needed
 };
 
-const defaultValue = '#'.repeat(card.numberLength).split('');
+const getDefaultValue = (numberLength: number) => '#'.repeat(numberLength).split('');
+
+// A custom hook or utility function to split the card number
+const useSplitCardNumber = (cardNumber: string, cardType: 'visa') => {
+    const card = cardConfig[cardType];
+
+    return useMemo(() => {
+        const cardNumberChars = cardNumber.split('');
+        const visualChars = getDefaultValue(card.numberLength).map((char, i) => cardNumberChars[i] || char);
+
+        const value: Array<string[]> = [];
+        card.parts.reduce((prev, acc) => {
+            value.push(visualChars.slice(prev, prev + acc));
+            return prev + acc;
+        }, 0);
+
+        return value;
+    }, [cardNumber, card]);
+};
 
 const CreditCardVisual = ({ className, formState }: CreditCardVisualProps) => {
     const { cardNumber, cardHolder, expirationMonth, expirationYear, cvv } = formState;
 
-    const splitCardNumber = useMemo(() => {
-        const cardNumberChars = cardNumber.split('');
-        const visualChars = defaultValue.map((char, i) => cardNumberChars[i] || char);
-
-        const value: Array<string[]> = [];
-
-        card.parts.reduce((prev, acc) => {
-            value.push(visualChars.slice(prev, acc + prev));
-            return (prev += acc);
-        }, 0);
-
-        return value;
-    }, [cardNumber]);
+    const splitCardNumber = useSplitCardNumber(cardNumber, 'visa'); // Assuming 'visa' as a default card type
 
     return (
         <div className={`card-visual ${className}`}>
@@ -37,13 +47,15 @@ const CreditCardVisual = ({ className, formState }: CreditCardVisualProps) => {
                 <div className='card-visual__type'></div>
             </div>
             <div className='card-visual__number'>
-                {splitCardNumber.map((cardNumberPart, i) => {
-                    return (
-                        <span key={`part-${i}`} className='card-visual__number__part'>
-                            {cardNumberPart.join('')}
-                        </span>
-                    );
-                })}
+                {splitCardNumber.map((part, i) => (
+                    <div key={`part-${i}`} className='card-visual__number__part'>
+                        {part.map((char, j) => (
+                            <span className='card-visual__number__part__char' key={`part-char-${j}`}>
+                                {char}
+                            </span>
+                        ))}
+                    </div>
+                ))}
             </div>
             <div>
                 <div className='card-visual__card-holder'>
@@ -52,9 +64,7 @@ const CreditCardVisual = ({ className, formState }: CreditCardVisualProps) => {
                 </div>
                 <div className='card-visual__expiry'>
                     <span>Expiry</span>
-                    <span>
-                        {expirationMonth}/{expirationYear}
-                    </span>
+                    <span>{`${expirationMonth}/${expirationYear}`}</span>
                 </div>
             </div>
         </div>
